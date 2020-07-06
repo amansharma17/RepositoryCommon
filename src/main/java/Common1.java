34ac48bb28704a9ad04a9177190e9b125bd7f683
Aman Sharma <amantopper@gmail.com>
2020-07-07 02:40:25 +0530

    sfdjjk

---
+++ src/main/java/Common1.java
@@ -0,0 +1,20 @@
+public class Common1{
+
+    public void commonHelper1(){
+        System.out.println("commonHelper1");
+    }
+
+    public int commonHelper2(){
+        System.out.println("commonHelper2");
+        return 1;
+    }
+
+    public String commonHelper3(){
+        System.out.println("commonHelper3");
+        return "abc";
+    }
+
+    public void commonHelper4(){
+        System.out.println("commonHelper4");
+    }
+}
---
+++ src/main/java/Common2.java
@@ -0,0 +1,20 @@
+public class Common2{
+
+    public void commonHelper21(){
+        System.out.println("commonHelper21");
+    }
+
+    public int commonHelper22(){
+        System.out.println("commonHelper22");
+        return 11;
+    }
+
+    public String commonHelper23(){
+        System.out.println("commonHelper23");
+        return "xyz";
+    }
+
+    public void commonHelper24(){
+        System.out.println("commonHelper24");
+    }
+}
